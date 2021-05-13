b0bd057a539c4bf6a9b6445a0c44a0bc95f40e2f
SkieHacker <40538401+SkieAdmin@users.noreply.github.com>
2021-05-13 18:30:52 +0800

    Update extract.cmd

--- Panda-Utilities/CefsharpUtilities/extract.cmd
+++ Panda-Utilities/CefsharpUtilities/extract.cmd
@@ -3,6 +3,6 @@ cls
 Title Panda 7-Zip
 cls
 echo Extracting Ceflib.dll... Please WAIT
-%USERPROFILE%\Panda.TechnologyCefSharp\7exe e %USERPROFILE%\Panda.TechnologyCefSharp\libcef.rar
+%USERPROFILE%\Panda.TechnologyCefSharp\7z.exe e %USERPROFILE%\Panda.TechnologyCefSharp\libcef.rar
 ping 127.0.0.1 -n 6 > nul
 exit
